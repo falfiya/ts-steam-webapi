a0de9d9b10e55a025924445c09b3755678f800ac
coalpha <22552895+coalpha@users.noreply.github.com>
2021-02-10 22:57:31 -0800

    /(?<=<(\w+)>)[^<]*(?=<\/\1>)/g
    programmers won't tell you about this one regex to make your error
    messages from bad Steam Web API calls up to 30% more readable!

--- src/api/call.ts
+++ src/api/call.ts
@@ -18,9 +18,20 @@ export async function api_call(
    const url = `https://${host}/${int}/${met}/${ver}?key=${this.key}&${params}`;
    const res = await fetch(url);
    if (res.status !== 200) {
-      const err = new Error(await res.text());
+      const err = new Error(render_html(await res.text()));
       err.name = `${res.status}, ${res.statusText}`;
       throw err;
    }
    return (await res.json()).response;
+}
+
+// https://stackoverflow.com/questions/1732348
+function render_html(html: string): string {
+   // match inner text of html tags
+   // man, this is beautiful
+   const inner_texts = html.match(/(?<=<(\w+)>)[^<]*(?=<\/\1>)/g);
+   if (inner_texts === null) {
+      return html;
+   }
+   return inner_texts.join(", ").replace(/[\s\n]+/g, ' ');
 }
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import {to_steam_id} from "./api/steam_id";
+import {to_steam_id, steam_id} from "./api/steam_id";
 import {steam_session} from "./steam_session";
 
 const key = require("../raw/key");
@@ -7,5 +7,5 @@ const instance = new steam_session(key);
 
 void async function main() {
    const coalpha_id = to_steam_id("76561198280673707");
-   const e = await instance.GetBadges(coalpha_id);
+   const e = await instance.GetBadges("e" as steam_id);
 }();
